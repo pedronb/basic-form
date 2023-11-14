ac6222968fbc4c472018a46b583a000987ddc7a2
Pedro Bezerra <pedro.bezerra@inovahu.com>
2023-11-14 18:08:03 -0300

    feat: create input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,18 @@
-import { Input as NativeBaseInput } from "native-base";
+import { Input as NativeBaseInput, IInputProps } from "native-base";
 
-export const Input: React.FC = () => {
-  return <NativeBaseInput bgColor={"gray.700"} />;
-};
+export function Input({ ...rest }: IInputProps) {
+  return (
+    <NativeBaseInput
+      bg="gray.700"
+      fontSize="md"
+      h={16}
+      {...rest}
+      mb={4}
+      _focus={{
+        bg: "gray.200",
+        borderWidth: 2,
+        borderColor: "green.500",
+      }}
+    />
+  );
+}
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,10 +4,14 @@ import { Input } from "../components/Input";
 
 export const SignUp: React.FC = () => {
   return (
-    <VStack bgColor={"gray.300"} flex={1}>
+    <VStack bgColor={"gray.300"} flex={1} px={10}>
       <Center>
         <Heading my={24}>Cria sua conta</Heading>
-        <Input />
+
+        <Input placeholder="Nome" />
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" />
+        <Input placeholder="Confirme a senha" />
       </Center>
     </VStack>
   );
